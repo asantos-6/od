6130d8eccb96595f255cb67a76d8b835eeb8cec5
André C. Santos <andresantos@dei.uc.pt>
2022-12-05 17:07:57 +0000

    typo in file

--- onset_detection/__init__.py
+++ onset_detection/__init__.py
@@ -11,7 +11,7 @@ song_dict = {
     "beatles": "../data/original_tracks/the_beatles-in_my_life.mp3",
     "queen": "../data/original_tracks/queen-another_one_bites_the_dust.mp3",
     "ac_dc": "../data/original_tracks/ac_dc-back_in_black.mp3",
-    "qotsa": "../data/original_tracks/qotsa_no_one_knows.mp3",
+    "qotsa": "../data/original_tracks/qotsa-no_one_knows.mp3",
     "slowj": "../data/original_tracks/slowj-mundança.mp3",
     "arctic_monkeys": "../data/original_tracks/arctic_monkeys-the_view_from_the_afternoon.mp3"
 }
