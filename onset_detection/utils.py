56ef683d2e2e313f6f75236be596933d916b6061
André C. Santos <andresantos@dei.uc.pt>
2022-12-12 14:48:56 +0000

    Normalize array function

--- onset_detection/utils.py
+++ onset_detection/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 from onset_detection.od import calculate_onset_times
 from onset_detection.audio import plot_audio, audio_seconds
@@ -23,3 +24,9 @@ def plot_combined(audio, sr, df_bins, df):
     plot_audio(audio, sr)
     onset_times = plot_df(audio, sr, df_bins, df, c='r', a=0.5, overlap=True)
     return onset_times
+
+# Normalize data
+def normalize_array(array):
+    scaler = MinMaxScaler()
+    array = scaler.fit_transform(array.reshape(-1, 1))
+    return array
